64bc6ebebb587cda3dbebc9ec3f48f668f83d5e5
efacelli <enzomagic32@gmail.com>
2023-08-31 19:20:39 -0300

    prueba

---
+++ main.js
@@ -0,0 +1,24 @@
+//array//
+
+const nombres = ["enzo", "luis", "nacho"];
+
+//push permite agregar al final//
+
+nombres.push("tomi");
+console.log(nombres);
+
+//unshift para agregar al inicio del arreglo//
+nombres.unshift("matias");
+
+//pop permite eliminar un elemento del final//
+nombres.pop();
+
+//shift para eliminar un elemento del principio//
+nombres.shift();
+
+//splice permite eliminar una cantidad segun los indiices
+const names = ["andres", "camila", "valentina"];
+names.splice(2, 1);
+
+
+
