28864febdbdc8cfc23045ffeaa9185d936411fcf
efacelli <enzomagic32@gmail.com>
2023-09-02 18:00:10 -0300

    correciones

---
+++ productos.js
@@ -0,0 +1,58 @@
+const productos = [
+	{
+		id: "1",
+		nombre: "Moto E32",
+		marca: "Motorola",
+		precio: 50,
+		descripcion: "El Motorola Moto E32 es un smartphone Android con una pantalla HD",
+	},
+	{
+		id: "2",
+		nombre: "Moto G22",
+		marca: "Motorola",
+		precio: 70,
+		descripcion: "Potenciado por un procesador MediaTek Helio",
+	},
+	{
+		id: "3",
+		nombre: "Moto G42",
+		marca: "Motorola",
+		precio: 75,
+		descripcion: "Smartphone Android. Potenciado por un procesador MediaTek Helio",
+	},
+	{
+		id: "4",
+		nombre: "Galaxy A34",
+		marca: "Samsung",
+		precio: 80,
+		descripcion: "Es el sucesor del Galaxy A33 5G. Con una pantalla Super AMOLED de 6.6 pulgadas",
+	},
+	{
+		id: "5",
+		nombre: "Galaxy M54",
+		marca: "Samsung",
+		precio: 80,
+		descripcion: "Se suma a la serie Galaxy M con una pantalla Super AMOLED",
+	},
+	{
+		id: "6",
+		nombre: "Galaxy S10e",
+		marca: "Samsung",
+		precio: 85,
+		descripcion: "Pantalla Full HD",
+	},
+	{
+		id: "7",
+		nombre: "Galaxy Tab S6 Lite",
+		marca: "Samsung",
+		precio: 90,
+		descripcion: "Pantalla Full HD",
+	},
+	{
+		id: "8",
+		nombre: "30 SE",
+		marca: "TCL",
+		precio: 95,
+		descripcion: "Buen celular",
+	},
+];
